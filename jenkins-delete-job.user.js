7bee735fbdff845d42995bc84147889c6b199ded
Rabin Yasharzadehe <rabin@rabin.io>
2023-11-29 14:03:53 +0200

    Update jenkins-delete-job.user.js

--- jenkins-delete-job.user.js
+++ jenkins-delete-job.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Delete Button for Jenkins Jobs
 // @namespace    https://rabin.io
-// @version      1.3.1
+// @version      1.4.3
 // @description  Adds a delete button to each row in a Jenkins pane table
 // @match        https://*/job/*/
 // @connect      self
@@ -11,13 +11,46 @@
 // @run-at       document-end
 // ==/UserScript==
 
-// Gist URL:    https://gist.github.com/rabin-io/72558495cbc1425eec2bbb57b798e78b
-// DownloadURL: https://gist.githubusercontent.com/rabin-io/72558495cbc1425eec2bbb57b798e78b/raw/jenkins-delete-job.user.js
+// URL:         https://github.com/rabin-io/userscripts/blob/dev/jenkins-delete-job.user.js
+// DownloadURL: https://github.com/rabin-io/userscripts/raw/dev/jenkins-delete-job.user.js
 
 (function() {
     'use strict';
-
     const baseURL = window.location.href; // Get the current page URL
+    const hostname = new URL(baseURL).hostname;
+
+    // Select all div elements with the class "task"
+    //var tasksDiv = document.querySelectorAll('div.task'); //:nth-child(3) > span:nth-child(1) > a:nth-child(1) > span:nth-child(2)
+    // Create a new div element
+    //var newDiv = document.createElement('div');
+
+    //Add Job clone button to the job page
+    var jobH1Header = document.querySelector('h1');
+    var jobTitle = jobH1Header.textContent.split(' ', 2)[1]
+
+    var cloneButton = document.createElement('button');
+    cloneButton.classList.add('jenkins-button--primary', 'jenkins-button');
+    cloneButton.textContent = "Clone Job";
+    cloneButton.addEventListener('click', () => {
+        let cloneURL = `//${hostname}/job/generate-test-job/parambuild/`;
+        let getParams = `ACCEPT=TRUE&JOB_NAME=${jobTitle}&REPO_OWNER=ryasharz&REPO_BRANCH=`;
+        cloneURL += `?${getParams}`;
+        window.location.href = cloneURL;
+    });
+    jobH1Header.parentNode.insertBefore(cloneButton, jobH1Header.nextSibling);
+
+    var paramBuildButton = document.createElement('button');
+    paramBuildButton.classList.add('jenkins-button--danger', 'jenkins-button');
+    paramBuildButton.textContent = "Param Build";
+    paramBuildButton.style.margin = '15px';
+    paramBuildButton.addEventListener('click', () => {
+        let paramBuildURL = `${baseURL}/parambuild/`;
+        window.location.href = paramBuildURL;
+    });
+    jobH1Header.parentNode.insertBefore(paramBuildButton, jobH1Header.nextSibling);
+
+
+
 
     // Find all table rows with the class "build-row" inside a table with the class "pane"
     const rows = document.querySelectorAll('table.pane tr.build-row');
@@ -34,12 +67,25 @@
         deleteButton.classList.add('jenkins-!-destructive-color'); // Add the class to the button
         deleteButton.textContent = 'Delete';
 
+        // Create a rebuild button
+        const rebuildButton = document.createElement('button');
+        // rebuildButton.classList.add('jenkins-!-destructive-color'); // Add the class to the button
+        rebuildButton.style.margin = '5px';
+        rebuildButton.textContent = 'Rebuild';
+
+        // Add a click event listener to the rebuild button
+        rebuildButton.addEventListener('click', () => {
+            //https://main-jenkins-csb-cnvqe.apps.ocp-c1.prod.psi.redhat.com/job/dev-deploy-cnv-4.15-on-aws-ipi-ryasharz/33/rebuild
+            const rebuildURL = `${jobLink}/rebuild`;
+            window.location.href = rebuildURL;
+        });
+
         // Add a click event listener to the delete button
         deleteButton.addEventListener('click', () => {
             const deleteURL = `${jobLink}/doDelete`; // Form the delete URL
             // {{API_SERVER}}/job/{{JOB_NAME}}/{{currentBuildId}}/doDelete/api/json/
-            //const deleteURL = `${jobLink}/doDelete/api/json/`; // Form the delete URL
-            console.log(deleteURL)
+            // const deleteURL = `${jobLink}/doDelete/api/json/`; // Form the delete URL
+            // console.log(deleteURL)
             // Send a POST request to the delete URL
             GM_xmlhttpRequest({
                 method: 'POST',
@@ -71,6 +117,6 @@
         const cell = row.querySelector('td');
         // Append the delete button to the cell
         cell.appendChild(deleteButton);
+        cell.appendChild(rebuildButton);
     }
 })();
-
